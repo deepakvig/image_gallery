c71c65c6d6fd02d2fa1d927fedde370c49eccda0
deepakvig@gmail.com <deepakvig@gmail.com>
2013-08-24 22:53:08 +0530

    Blog index page updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,15 @@
 class ApplicationController < ActionController::Base
+  include ApplicationHelper
   protect_from_forgery
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
   end
-
+  
   def login_required
-    if user_signed_in?
-      return false
+    if !user_signed_in?
+      redirect_to '/'
     end
-    return true
   end
+    
 end
